2f73794b2c0e845a107d8e4da8c9ec3e53a81bd7
IslandIdealist <IslandIdealist@gmail.com>
2020-09-27 16:04:32 -0500

    uploading base swift configs for Zones.

---
+++ Zones/Zones/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zones
+//
+//  Created by Matt Jerrard on 9/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to Zones!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Zones/Zones/ZonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZonesApp.swift
+//  Zones
+//
+//  Created by Matt Jerrard on 9/27/20.
+//
+
+import SwiftUI
+
+@main
+struct ZonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
